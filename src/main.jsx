87d08937c36fb8f7aa376ccc48cff7ead2b1d7aa
Jose luis <boxixz0991@gmail.com>
2023-05-26 15:29:09 +0200

    react 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 import './styles/index.css';
 
+// const container = document.getElementById('root');
+// ReactDOM.render(<App />, container);
+
 const container = document.getElementById('root');
-ReactDOM.render(<App />, container);
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App></App>);
